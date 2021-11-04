7893e4522deeef51ba3d5c84faa67d0a5f83fe17
ch4rli3kop <pch2180@gmail.com>
2021-11-04 18:16:19 +0900

    사용자 닉네임을 통한 채팅방검색 기능 추가

--- app/src/main/java/com/example/project/ChatListFragment.kt
+++ app/src/main/java/com/example/project/ChatListFragment.kt
@@ -1,6 +1,7 @@
 package com.example.project
 
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,24 +19,42 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class ChatListFragment : Fragment() {
 
-    private var rootChatRef : DatabaseReference = FirebaseDatabase.getInstance("https://food09-581c6-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Chat")
+class ChatListFragment : Fragment() {
+    private val rootRef : FirebaseDatabase = FirebaseDatabase.getInstance("https://food09-581c6-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    private val rootChatRef : DatabaseReference = rootRef.getReference("Chat")
+    private val chatUserRef : DatabaseReference = rootRef.getReference("ChatUser")
     private lateinit var chatRef : DatabaseReference
     private var chatList = arrayListOf<ChatModel>()
+    private lateinit var chatChannelKey : String
     private lateinit var chatAdapter : ChatAdapter
-    private lateinit var currentNickName: String
+    private var currentNickName: String = "charlie"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+//        init()
     }
 
-    fun findChat() : Boolean {
-        // 현재 유저의 닉네임으로 참여하고 있는 채팅방 찾기
-        chatRef = rootChatRef.child("chat1")
+//    fun init(){
+//        val user : DatabaseReference = chatUserRef.child(currentNickName)
+//        if (user == null) {
+//            Log.d("ChatListFragment", "Failed to find the user in ChatUser")
+//        }
+//
+//        user.addListenerForSingleValueEvent(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                chatKey = snapshot.value.toString()
+//                Log.d("ChatListFragment", chatKey.toString())
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.d("ChatListFragment", "Failed to find the user in ChatUser (reading data)")
+//            }
+//        })
+//    }
 
-        return true
+    fun findChat() : Boolean {
+        return false
     }
 
     override fun onCreateView(
@@ -52,10 +71,10 @@ class ChatListFragment : Fragment() {
         val chatContent = rootView.findViewById(R.id.et_chatting) as TextView
 
         // Todo: 현재 유저가 있는 채팅방 찾기 -> chatRef 초기화
-        if ( findChat() == false ){
-            notify.text = "공구에 참여했을 경우에 채팅창이 활성화됩니다."
-            return rootView
-        }
+//        if ( findChat() == false ){
+//            notify.text = "공구에 참여했을 경우에 채팅창이 활성화됩니다."
+//            return rootView
+//        }
 
         notify.text = ""
 
@@ -97,29 +116,73 @@ class ChatListFragment : Fragment() {
         return rootView
     }
 
-    fun readChat(){
-//        chatList.clear()
-//        chatAdapter.notifyDataSetChanged()
-        val chatQuery = chatRef.orderByChild("dateTime")
+    private fun readChat(){
+        // find key
+        // 현재 유저의 닉네임으로 참여하고 있는 채팅방 찾기
+//        Log.d("ChatListFragment !!! chatKey ! ", chatKey.toString())
 
-        chatQuery.addValueEventListener(object: ValueEventListener {
+
+//        val user = chatUserRef.equalTo(currentNickName)
+        val user = chatUserRef.child(currentNickName)
+//
+        user.addListenerForSingleValueEvent(object: ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-//                var tmpChatList = ArrayList<ChatModel>()
-                chatList.clear()
-                for ( chat in snapshot.children) {
-                    Log.d("ChatListFragment", chat.key.toString())
-                    val chatKey : String = chat.key.toString()
-                    val nickName : String = chat.child("nickName").value.toString()
-                    val content : String = chat.child("content").value.toString()
-                    val dataTime : String = chat.child("dateTime").value.toString()
-//                    tmpChatList.add(ChatModel(chatKey, nickName, content, dataTime))
-                    chatList.add(ChatModel(chatKey, nickName, content, dataTime))
-                }
-//                chatList.reverse()
-//                chatList = tmpChatList
-//                chatAdapter.replaceList(chatList)
-                chatAdapter.notifyDataSetChanged()
+                chatChannelKey = snapshot.value.toString()
+                Log.d("ChatListFragment chatChannelKey", chatChannelKey)
+                chatRef = rootChatRef.child(chatChannelKey)
+
+                val chatQuery = chatRef.orderByChild("dateTime")
+
+                chatQuery.addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        chatList.clear()
+                        for (chat in snapshot.children) {
+                            Log.d("ChatListFragment", chat.key.toString())
+                            val chatKey: String = chat.key.toString()
+                            val nickName: String = chat.child("nickName").value.toString()
+                            val content: String = chat.child("content").value.toString()
+                            val dataTime: String = chat.child("dateTime").value.toString()
+                            chatList.add(ChatModel(chatKey, nickName, content, dataTime))
+                        }
+                        chatAdapter.notifyDataSetChanged()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.d(
+                            "ChatListFragment",
+                            "Failed to find the user in ChatUser (reading data)"
+                        )
+                    }
+                })
             }
+//
+//        Log.d("ChatListFragment chatKey ! ", user.key toString())
+//        user.
+//        Log.d("ChatListFragment chatKey ! ", user.get().toString())
+//        Log.d("ChatListFragment", "Find the chatKey in ChatUser! " + chatRef.toString())
+
+
+//        chatRef = rootChatRef.child("chat1")
+//        val chatQuery = chatRef.orderByChild("dateTime")
+//
+//        chatQuery.addValueEventListener(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+////                var tmpChatList = ArrayList<ChatModel>()
+//                chatList.clear()
+//                for ( chat in snapshot.children) {
+//                    Log.d("ChatListFragment", chat.key.toString())
+//                    val chatKey : String = chat.key.toString()
+//                    val nickName : String = chat.child("nickName").value.toString()
+//                    val content : String = chat.child("content").value.toString()
+//                    val dataTime : String = chat.child("dateTime").value.toString()
+////                    tmpChatList.add(ChatModel(chatKey, nickName, content, dataTime))
+//                    chatList.add(ChatModel(chatKey, nickName, content, dataTime))
+//                }
+////                chatList.reverse()
+////                chatList = tmpChatList
+////                chatAdapter.replaceList(chatList)
+//                chatAdapter.notifyDataSetChanged()
+//            }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.d("ChatListFragment", "Failed to read Database")
