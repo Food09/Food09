026737967279fb0a7ff38f0615497cbda0c66663
ch4rli3kop <pch2180@gmail.com>
2021-11-04 23:43:19 +0900

    채팅데이터 없을 때는 메시지 전송이 안되게 막음

--- app/src/main/java/com/example/project/ChatListFragment.kt
+++ app/src/main/java/com/example/project/ChatListFragment.kt
@@ -2,6 +2,7 @@ package com.example.project
 
 import android.os.Bundle
 import android.provider.ContactsContract
+import android.text.TextUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.*
@@ -101,6 +103,10 @@ class ChatListFragment : Fragment() {
 
         btnSend.setOnClickListener {
             Log.d("ChatListFragment", "btn clicked!")
+
+            if (TextUtils.isEmpty(et_chatting.text.toString().trim())){
+                return@setOnClickListener
+            }
 
             // add chat data to firebase
             val chatKey : String? = chatRef.push().key
