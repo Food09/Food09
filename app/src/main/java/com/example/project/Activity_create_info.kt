92fe89ad8d7779e0946f456170d07da588ed8287
ch4rli3kop <pch2180@gmail.com>
2021-11-05 00:08:29 +0900

    회원가입 시 동일한 별명을 사용하지 못하도록 막음

--- app/src/main/java/com/example/project/Activity_create_info.kt
+++ app/src/main/java/com/example/project/Activity_create_info.kt
@@ -12,8 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_create_info.*
 import java.text.SimpleDateFormat
@@ -57,38 +56,65 @@ class Activity_create_info : AppCompatActivity() {
     }
 
     private fun signUp(email: String, password: String, displayName: String, displayName_check:String, my_info:String) {
-        if (email.isNotEmpty() and password.isNotEmpty()) {
-            if (displayName == displayName_check) {
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success")
-                            val user = auth.currentUser
-                            updateUI( user, email, displayName, my_info)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(
-                                baseContext, "이메일 또는 비밀번호를 다시 확인해주세요.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            // updateUI(null)
-                        }
+
+        if (email.isEmpty() or password.isEmpty()) {
+            Toast.makeText(baseContext, "아이디 또는 패스워드를 입력해주세요.",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (displayName != displayName_check) {
+            Toast.makeText(baseContext, "별명을 다시 확인해주세요.",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        val userRef : DatabaseReference = FirebaseDatabase.getInstance("https://food09-581c6-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("User")
+        val userQuery : Query = userRef.orderByChild("nickName")
+        Log.d( "CreateInfo", userQuery.toString())
+
+        userQuery.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var tmpUserName : String? = null
+                for (user in snapshot.children) {
+                    val userNickName: String = user.child("nickName").value.toString()
+                    Log.d("MainActivity", userNickName)
+                    if (userNickName == displayName) {
+                        tmpUserName = userNickName
+                        break
                     }
-            } else {
-                Toast.makeText(
-                    baseContext, "별명을 다시 확인해주세요.",
-                    Toast.LENGTH_SHORT
-                ).show()
+                }
+                if (tmpUserName == null){
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this@Activity_create_info) { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success")
+                                val user = auth.currentUser
+                                updateUI( user, email, displayName, my_info)
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                                Toast.makeText(
+                                    baseContext, "이메일 또는 비밀번호를 다시 확인해주세요.",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                return@addOnCompleteListener
+                                // updateUI(null)
+                            }
+                        }
+                } else { // 동일한 별명이 서버에 등록되어 있는 경우
+                    Toast.makeText(
+                        baseContext, "동일한 별명을 사용하고 있는 사람이 있습니다!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return
+                }
             }
-        }
-        else{
-            Toast.makeText(
-                baseContext, "이메일 또는 비밀번호를 다시 확인해주세요.",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("CreateInfo", "Failed to read Database")
+            }
+        })
 
 
     }
