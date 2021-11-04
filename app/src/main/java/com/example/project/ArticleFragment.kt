486ef1586b615679abef3e1f473994ac8470bce1
ch4rli3kop <pch2180@gmail.com>
2021-11-05 00:31:55 +0900

    참여버튼 클릭시 (사용자, articleKey) 정보를 CharUser에 추가

--- app/src/main/java/com/example/project/ArticleFragment.kt
+++ app/src/main/java/com/example/project/ArticleFragment.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ArticleFragment : Fragment() {
-
+    val rootDB : FirebaseDatabase = FirebaseDatabase.getInstance("https://food09-581c6-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    lateinit var article : ArticleModel
+    lateinit var userInfo : UserModel
 //    private lateinit var callback: OnBackPressedCallback
 //
 //    override fun onAttach(context: Context) {
@@ -47,13 +51,28 @@ class ArticleFragment : Fragment() {
         if (getArguments() != null){
 //            var data : String? = requireArguments().getString("test")
 //            Log.d("ArticleFragment", "data : " + data)
-            val article : ArticleModel = requireArguments()!!.getSerializable("articleInfo") as ArticleModel
+            article = requireArguments()!!.getSerializable("articleInfo") as ArticleModel
             Log.d("ArticleFragment", "ArticleInfo : " + article.get_userID())
+            userInfo = requireArguments()!!.getSerializable("userInfo") as UserModel
+
             userID.text = article.get_userID()
             userProfile.text = article.get_userProfile()
             title.text = article.get_title()
             content.text = article.get_content()
             userNumber.text = article.get_curNum().toString() + " / " + article.get_maxNum().toString()
+        }
+
+        enterButton.setOnClickListener {
+            Log.d("ArticleFragment", "enterButton Clicked!")
+            // (현재사용자, articleKey) 데이터를 ChatUser Firebase에 추가하기
+            val chatUserRef : DatabaseReference = rootDB.getReference("ChatUser")
+            chatUserRef.child(userInfo.nickName).setValue(article.articleKey)
+
+            // ToDo: Chat 디비의 멤버에 추가함
+
+
+            // ToDo: Firebase 접속해서 인원수 변경
+
         }
 
         return rootView
